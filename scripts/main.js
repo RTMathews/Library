a6d5484c7a329d1c468d34aa235c5dab0e1a440e
Ryan <mathewsthomasryan@gmail.com>
2024-03-22 05:34:58 -0500

    addbook

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,6 @@ function addBookToLibrary() {
 let newBookBtn = document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click", function() {
     let newBookForm = document.querySelector("#new-book-form");
-    newBookForm.computedStyleMap.display = block;
+    console.log(newBookForm);
+    newBookForm.style.display = "block";
 })
